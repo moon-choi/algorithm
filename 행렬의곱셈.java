30dfde610b0aa71dd948f691ea82ab1d439d10d9
moon-choi <choimy1991@gmail.com>
2024-03-28 17:26:34 +0900

    행렬의 곱셈

--- 행렬의곱셈.java
+++ 행렬의곱셈.java
@@ -1,38 +1,21 @@
+import java.util.Arrays;
+
 public class 행렬의곱셈 {
-  public static void main(String[] args) {
-    int[][] arr1 = {{2, 3, 2}, {4, 2, 4}, {3, 1, 4}};
-    int[][] arr2 =	{{5, 4, 3}, {2, 4, 1}, {3, 1, 1}};
-    int[][] answer = {{22, 22, 11}, {36, 28, 18}, {29, 20, 14}};
-    int[][] result = new int[arr1.length][arr2.length];
-    int crossed = 0;
-    int multiplied = 0;
-    int sum = 0;
-    int added = 0;
-    for(int i = 0; i < arr1.length; i++) {
 
-      for(int j = 0; j < arr1[i].length; j++){
+    public static void main(String[] args) {
+        int[][] arr1 = {{2, 3, 2}, {4, 2, 4}, {3, 1, 4}};
+        int[][] arr2 = {{5, 4, 3}, {2, 4, 1}, {3, 1, 1}};
+        int[][] answer = new int[arr1.length][arr2[0].length];
 
-        for(int k = 0; k < arr2[j].length; k++) {
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
 
-
-
-          multiplied = arr1[i][j] * arr2[j][i];
-          sum += multiplied;
-          System.out.println(sum);
+                }
+            }
         }
 
-//                System.out.println(multiplied);
-//                System.out.println(i + "," + j + " * " + j + "," + i);
-
-      }
-
-      System.out.println("==");
-
-
-
-
+        System.out.println(Arrays.deepToString(answer));
     }
-
-    //arr1에서 첫덩어리가 arr2의 모든 판을 다 돌고나면 result의 한 줄이 채워짐.
-  }
 }
