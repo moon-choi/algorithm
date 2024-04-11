e07c7c93ecade217d839d4b08c5a8defea0afc47
moon-choi <choimy1991@gmail.com>
2024-04-11 15:43:13 +0900

    풀이 2 추가

--- 콜라츠추측.java
+++ 콜라츠추측.java
@@ -13,6 +13,7 @@ public class 콜라츠추측 {
         sol.solution(626331);
     }
 
+    //풀이 1.
     public int solution(long num) {
 
         int answer = 0;
@@ -32,7 +33,34 @@ public class 콜라츠추측 {
                 num = num * 3 + 1;
                 answer++;
             }
+            answer = answer;
         }
+        System.out.println(answer);
+        return answer;
+    }
+
+
+    //풀이 2.
+    public int solution2(long num) {
+
+        int answer = 0;
+
+        //1이면 아예 while 문으로 들어가지 않음.
+        while (answer < 500 && num > 1) {
+            answer++; //먼저 올려줌. 그러고 나서 500 이상인지 검사해야함.
+            if (answer >= 500) {
+                answer = -1;
+                break;
+            }
+
+            if (num % 2 == 0) { //짝수일 때
+                num /= 2;
+
+            } else { //홀수일 때
+                num = num * 3 + 1;
+            }
+        }
+//        answer++; //여기서 올리면 안됌. 그러면 500이 리턴될 수 있음.
         System.out.println(answer);
         return answer;
     }
