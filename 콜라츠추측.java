e4263cf26b4830a691bf87f0339dfaef5c8ce3fd
moon-choi <choimy1991@gmail.com>
2024-04-11 15:37:29 +0900

    콜라츠추측

--- 콜라츠추측.java
+++ 콜라츠추측.java
@@ -7,33 +7,31 @@ public class 콜라츠추측 {
 
     public static void main(String[] args) {
         콜라츠추측 sol = new 콜라츠추측();
+        sol.solution(1);
         sol.solution(6);
         sol.solution(16);
         sol.solution(626331);
     }
 
     public int solution(long num) {
-        int answer = 0;
-        int count = 0;
 
-        while (count < 500 && num > 1) {
-            if (num % 2 == 0) {
+        int answer = 0;
+
+        //1이면 아예 while 문으로 들어가지 않음.
+        while (answer < 500 && num > 1) {
+
+            if (num % 2 == 0) { //짝수일 때
                 num /= 2;
-                count++;
-                if (count >= 500) {
+                answer++; //count가 499번째면 일단 while문 들어오고,
+                if (answer >= 500) { //1 올려준 뒤 break.
                     answer = -1;
                     break;
                 }
-            } else {
-                num = num * 3 + 1;
-                count++;
-                if (num == 1) {
-                    answer = 0;
-                    break;
-                }
 
+            } else { //홀수일 때
+                num = num * 3 + 1;
+                answer++;
             }
-            answer = count;
         }
         System.out.println(answer);
         return answer;
