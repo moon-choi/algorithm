db4901eaf7a31062ac9ff58d50572cdd6d1495c0
moon-choi <47645162+moon-choi@users.noreply.github.com>
2024-04-17 16:25:49 +0900

    [level 2] Title: 타겟 넘버, Time: 0.74 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,23 @@
+class Solution {
+    
+    static int answer = 0;
+    
+    public static int solution(int[] numbers, int target) {
+
+        dfs(0, 0, numbers, target);
+        System.out.println(answer);
+        return answer;
+    }
+
+    private static void dfs(int sum, int idx, int[] numbers, int target) {
+        if (idx == numbers.length && sum == target) {
+            answer++;
+            return;
+        }
+        if (idx >= numbers.length) {
+            return;
+        }
+        dfs(sum + numbers[idx], idx + 1, numbers, target);
+        dfs(sum - numbers[idx], idx + 1, numbers, target);
+    }
+}
