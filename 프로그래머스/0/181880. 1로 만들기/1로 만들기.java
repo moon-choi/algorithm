c75ac5177e04db0000a16e40bf3c28a89ed733a8
moon-choi <47645162+moon-choi@users.noreply.github.com>
2024-04-17 14:56:21 +0900

    [level 0] Title: 1로 만들기, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int count = 0;
+        
+         for(int num : num_list) {
+          while(num != 1) {
+            if (num % 2 == 0) {
+              num = num / 2;
+              count++;
+            } else {
+              num = (num - 1) / 2;
+              count++;
+            }
+          }
+        } 
+        
+        return count;
+    }
+}
