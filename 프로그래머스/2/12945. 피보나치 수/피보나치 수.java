3ceebecaf6de657b76e4eb8f6028d01f13012985
moon-choi <47645162+moon-choi@users.noreply.github.com>
2024-04-25 18:13:36 +0900

    [level 2] Title: 피보나치 수, Time: 2.52 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,16 +1,17 @@
 class Solution {
-    public static int solution(int n) {
+    public static long solution(int n) {
         int answer = 0;
-        
-        int[] arr = new int[n+1];
+
+//        int[] arr = new int[100000]; //런타임 에러
+        int[] arr = new int[n + 1];
         arr[0] = 0;
         arr[1] = 1;
 
         for (int i = 2; i <= n; i++) {
-           arr[i] = ( (arr[i - 1] % 1234567) + (arr[i - 2] % 1234567) ) % 1234567;
-            // arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+//            arr[i] = ((arr[i - 1] % 1234567) + (arr[i - 2] % 1234567)) % 1234567;
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
             answer = arr[i];
         }
-        return (int) answer;
+        return answer;
     }
 }
